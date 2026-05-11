a4d667cda817870bcbdcb8e03cf4a55905a211e1
scriptpgcluster <scriptpgcluster@gmail.com>
2026-05-11 08:50:23 +0200

    pgcluster v.2.0 comunity
    Comunity version 2.0 . Repo is restarted an bugs fixed

---
+++ pg_env.sh
@@ -0,0 +1,8 @@
+PATH=/opt/Postgresql/bin:$PATH
+PGDATA=/opt/Postgresq/data
+PGDATABASE=postgres
+PGUSER=postgres
+PGPORT=16432
+PGLOCALEDIR=/opt/Postgresql/share/locale
+MANPATH=$MANPATH:/opt/Postgresql/share/man
+LD_LIBRARY_PATH=/opt/Postgresql/lib:$LD_LIBRARY_PATH

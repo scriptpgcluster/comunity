d9ea49f6b20ecbf06d0701a2a257e802e6e7bf25
scriptpgcluster <scriptpgcluster@gmail.com>
2026-05-27 13:44:49 +0200

    Scriptpgcluster v2.0 Fix login IP
    Scriptpgcluster v2.0 Fix login IP

--- scripts/database/preload_postgres_ddl.sh
+++ scripts/database/preload_postgres_ddl.sh
@@ -3,7 +3,4 @@
 #Header environment
 . /opt/Postgresql/config/scripts/romerarioCluster.environment
 
-#Ejemplo
-#psql -h ${CLUSTERIP} -p ${CLUSTER_PORT} -U ${USERDB} -d ${DBNAME} -f ${PGSCRIPTSDIR}/database/ddl_exe_siva_20191112.sql >> ${DB_LOG_DIR}/backup.log
-
 exit 0

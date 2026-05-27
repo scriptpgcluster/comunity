dea07a17350f9d7f5a1d87c64ddf4dc3da87cfd7
scriptpgcluster <scriptpgcluster@gmail.com>
2026-05-27 13:43:09 +0200

    Scriptpgcluster v2.0 Fix login IP
    Scriptpgcluster v2.0 Fix login IP

--- scripts/database/sql/createDBRolTableSpace.sql
+++ scripts/database/sql/createDBRolTableSpace.sql
@@ -1,7 +1,7 @@
 -- Create roles and tableSpace
 
 CREATE ROLE admea LOGIN PASSWORD 'admin' SUPERUSER NOINHERIT CREATEDB;
-CREATE TABLESPACE ea1 OWNER postgres LOCATION '/opt/Postgresql/admintable';
+CREATE TABLESPACE admintable OWNER postgres LOCATION '/opt/Postgresql/admintable';
 GRANT CREATE ON TABLESPACE ea1 TO admin;
 CREATE ROLE replicator_bbdd LOGIN REPLICATION ENCRYPTED PASSWORD 'replicator_bbdd';
 GRANT pg_monitor TO replicator_bbdd;
